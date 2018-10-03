86ca0405190ff425287a01c3e5a0ef71a6652816
AdamKoni <az_k@gazeta.pl>
2018-10-03 18:38:32 +0200

    finished chapter 3 of Spring MVC without last extra work

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -1,8 +1,7 @@
 package com.packt.webstore.domain.repository.impl;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.stereotype.Repository;
 import com.packt.webstore.domain.Product;
@@ -11,7 +10,7 @@ import com.packt.webstore.domain.repository.ProductRepository;
 @Repository
 public class InMemoryProductRepository implements ProductRepository {
     private List<Product> listOfProducts = new ArrayList<Product>();
-
+    
     public InMemoryProductRepository() {
         Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
         iphone.setDescription("Apple iPhone 5s, smartfon z 4-calowym ekranem o rozdzielczości 6401136 i 8-megapikselowym aparatem");
@@ -32,11 +31,11 @@ public class InMemoryProductRepository implements ProductRepository {
         listOfProducts.add(laptop_dell);
         listOfProducts.add(tablet_Nexus);
     }
-
+    
     public List<Product> getAllProducts() {
         return listOfProducts;
     }
-
+    
     @Override
     public Product getProductById(String productId) {
         Product productById = null;
@@ -50,5 +49,38 @@ public class InMemoryProductRepository implements ProductRepository {
             throw new IllegalArgumentException("Brak produktu o wskazanym id: " + productId);
         }
         return productById;
+    }
+    
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        List<Product> productsByCategory = new ArrayList<Product>();
+        for (Product product : listOfProducts) {
+            if (category.equalsIgnoreCase(product.getCategory())) {
+                productsByCategory.add(product);
+            }
+        }
+        return productsByCategory;
+    }
+    
+    public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
+        Set<Product> productsByBrand = new HashSet<Product>();
+        Set<Product> productsByCategory = new HashSet<Product>();
+        Set<String> criterias = filterParams.keySet();
+        if (criterias.contains("brand")) {
+            for (String brandName : filterParams.get("brand")) {
+                for (Product product : listOfProducts) {
+                    if (brandName.equalsIgnoreCase(product.getManufacturer())) {
+                        productsByBrand.add(product);
+                    }
+                }
+            }
+        }
+        if (criterias.contains("category")) {
+            for (String category : filterParams.get("category")) {
+                productsByCategory.addAll(this.getProductsByCategory(category));
+            }
+        }
+        productsByCategory.retainAll(productsByBrand);
+        return productsByCategory;
     }
 }
--- src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
@@ -7,15 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
+    
     @Autowired
     private ProductRepository productRepository;
-
+    
     @Override
     public List<Product> getAllProducts() {
         return productRepository.getAllProducts();
+    }
+    
+    @Override
+    public Product getProductById(String productId) {
+        return productRepository.getProductById(productId);
+    }
+    
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.getProductsByCategory(category);
+    }
+    
+    @Override
+    public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
+        return productRepository.getProductsByFilter(filterParams);
     }
 }
